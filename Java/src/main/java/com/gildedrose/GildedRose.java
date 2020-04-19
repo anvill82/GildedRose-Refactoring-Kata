7dcd7f989a7b655a933f7bc5e6a63cff6f05a0a1
ivailo <ivailo.s.stanev@gmail.com>
2020-04-19 16:22:05 +0300

    Inverted all negative IF logic

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -23,14 +23,7 @@ class GildedRose {
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
-      if (!items[i].name.equals(AGED_BRIE)
-              && !items[i].name.equals(CONCERT_PASS)) {
-        if (items[i].quality > 0) {
-          if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
-            items[i].quality = items[i].quality - 1;
-          }
-        }
-      } else {
+      if (items[i].name.equals(AGED_BRIE) || items[i].name.equals(CONCERT_PASS)) {
         if (items[i].quality < 50) {
           items[i].quality = items[i].quality + 1;
 
@@ -48,26 +41,34 @@ class GildedRose {
             }
           }
         }
+      } else {
+        if (items[i].quality > 0) {
+          if (items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+          } else {
+            items[i].quality = items[i].quality - 1;
+          }
+        }
       }
 
-      if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+      if (items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
+      } else {
         items[i].sellIn = items[i].sellIn - 1;
       }
 
       if (items[i].sellIn < 0) {
-        if (!items[i].name.equals(AGED_BRIE)) {
-          if (!items[i].name.equals(CONCERT_PASS)) {
+        if (items[i].name.equals(AGED_BRIE)) {
+          if (items[i].quality < 50) {
+            items[i].quality = items[i].quality + 1;
+          }
+        } else {
+          if (items[i].name.equals(CONCERT_PASS)) {
+            items[i].quality = 0;
+          } else {
             if (items[i].quality > 0) {
               if (!items[i].name.equals(SULFURAS_HAND_OF_RAGNAROS)) {
                 items[i].quality = items[i].quality - 1;
               }
             }
-          } else {
-            items[i].quality = items[i].quality - items[i].quality;
-          }
-        } else {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
           }
         }
       }
